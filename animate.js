2507220c4d2b726eaa58c5ebe3f8f4d62c674df4
wrek <129691950+wrekafekt@users.noreply.github.com>
2023-07-19 14:04:23 -0500

    Update animate.js

--- animate.js
+++ animate.js
@@ -1,9 +1,7 @@
 var controller = new ScrollMagic.Controller();
 
-var tween = TweenMax.to("peek.jpg", 1, {
-    x: "100%"
-});
+var tween = TweenMax.to("#animate img", 1, {left: "0%"});
 
-var scene = new ScrollMagic.Scene({triggerElement: "peek.jpg", duration: "100%"})
+var scene = new ScrollMagic.Scene({triggerElement: "#animate", duration: "100%"})
                 .setTween(tween)
                 .addTo(controller);
