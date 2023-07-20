d92565ccda1aa6fdbd4c2e5ae5b67e39b1586562
wrek <129691950+wrekafekt@users.noreply.github.com>
2023-07-19 19:05:37 -0500

    Update animate.js

--- animate.js
+++ animate.js
@@ -1,7 +1,8 @@
-var controller = new ScrollMagic.Controller();
-
-var tween = TweenMax.to("#animate img", 1, {left: "0%"});
-
-var scene = new ScrollMagic.Scene({triggerElement: "#animate", duration: "100%"})
-                .setTween(tween)
-                .addTo(controller);
+function scrollAnimation() {
+    var controller = new ScrollMagic.Controller();
+    var tween = TweenMax.to("#animate img", 1, {left: "0%"});
+    var scrollScene = new ScrollMagic.Scene({triggerElement: "#animate", duration: "100%"})
+                    .setTween(tween)
+                    .addTo(controller);
+}
+scrollAnimation();
