06f761135c6fa93a8b6ec7130c0f1d0d5ecd2cd4
wrek <129691950+wrekafekt@users.noreply.github.com>
2023-07-21 15:12:48 -0500

    Update animate.js

--- animate.js
+++ animate.js
@@ -3,7 +3,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     var tween = TweenMax.to("#animate img", 1, {left: "0%"});
 
-    var scene = new ScrollMagic.Scene({triggerElement: "#animate", duration: "100%"})
-                    .setTween(tween)
-                    .addTo(controller);
+// Create a new ScrollMagic Scene
+    var scene = new ScrollMagic.Scene({
+      triggerElement: '.middle-image', // Specifies the element that triggers the animation
+      duration: '50%', // The animation will last for 50% of the scroll duration
+      triggerHook: 0.5, // The animation will start when the trigger element hits the middle of the viewport
+})
+.setPin('.middle-image') // Pins the element for the duration of the animation
+.addTo(controller); // Adds the scene to the controller
+
+// Use the onEnter event to start the animation
+scene.on('enter', function() {
+  document.querySelector('.middle-image').style.transform = 'translateX(50vw)';
 });
