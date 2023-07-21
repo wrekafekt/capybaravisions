73fb02a5e16ec55ee14c3b11d4e0c0b6318849d1
wrek <129691950+wrekafekt@users.noreply.github.com>
2023-07-21 11:58:59 -0500

    Create NftCarousel.svelte

---
+++ NftCarousel.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount } from 'svelte';
+  let images = ['peek.jpg', 'peek.jpg', 'peek.jpg']; // replace with NFT images
+  let currentImageIndex = 0;
+  let intervalId;
+
+  const cycleImages = () => {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+  };
+
+  onMount(() => {
+    intervalId = setInterval(cycleImages, 3000); // Change image every 3 seconds
+    return () => {
+      clearInterval(intervalId);
+    };
+  });
+</script>
+
+<div class="carousel">
+  <img src={images[currentImageIndex]} alt="Current NFT" />
+</div>
